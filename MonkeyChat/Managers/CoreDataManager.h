d4ffd5de7aff643d87be71cfcedfd2eeae225c40
Elvin Rakhmankulov <erakhmankulov@sapient.com>
2015-03-21 10:00:12 -0500

    Added CoreDataManager

---
+++ MonkeyChat/Managers/CoreDataManager.h
@@ -0,0 +1,20 @@
+//
+//  CoreDataManager.h
+//  MonkeyChat
+//
+//  Created by Elvin Rakhmankulov on 3/21/15.
+//  Copyright (c) 2015 Elvin Rakhmankulov. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <CoreData/CoreData.h>
+
+@interface CoreDataManager : NSObject
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+
+@end
